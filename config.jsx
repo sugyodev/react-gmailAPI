5958ad48f966724c8dc522e4dad66643a67eb9bf
~ Crazy Programmar ~ <109362542+sugyodev@users.noreply.github.com>
2023-04-06 06:02:02 +1000

    2

--- config.jsx
+++ config.jsx
@@ -1,5 +1,4 @@
 export const config = {
     CLIENT_ID: '', // Must be taken from Google API Console (https://console.developers.google.com/)
-    
     API_KEY: '' // https://developers.google.com/maps/documentation/javascript/get-api-key
 }
