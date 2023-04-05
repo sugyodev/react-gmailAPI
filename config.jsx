3e0a2e98c76f9d4bde467ad18878b61ec71c05a6
~ Crazy Programmar ~ <109362542+sugyodev@users.noreply.github.com>
2023-04-06 05:59:07 +1000

    first commit

--- config.jsx
+++ config.jsx
@@ -1,4 +1,5 @@
 export const config = {
     CLIENT_ID: '', // Must be taken from Google API Console (https://console.developers.google.com/)
+    
     API_KEY: '' // https://developers.google.com/maps/documentation/javascript/get-api-key
 }
