8abe96389a54b3b06606a2f92b77f610bedfc792
~ Crazy Programmar ~ <109362542+sugyodev@users.noreply.github.com>
2023-04-06 01:47:55 +1000

    second commit

--- config.jsx
+++ config.jsx
@@ -1,4 +1,4 @@
 export const config = {
-    CLIENT_ID: '961925666953-9c3hpq3nqg4dql8rjm98abs9fd21hhe5.apps.googleusercontent.com', // Must be taken from Google API Console (https://console.developers.google.com/)
-    API_KEY: 'AIzaSyBohOXl-C_F5_wq77eQBWN4e2vLICz6LJo' // https://developers.google.com/maps/documentation/javascript/get-api-key
+    CLIENT_ID: '', // Must be taken from Google API Console (https://console.developers.google.com/)
+    API_KEY: '' // https://developers.google.com/maps/documentation/javascript/get-api-key
 }
